e5e8cef8914d49aa82f6b17bdeb34ab5e4d4740a
Andrew Yourtchenko <ayourtch@gmail.com>
2016-08-22 13:07:26 +0000

    Fix the infinite loop on certain inputs
    (https://github.com/m-schmoock/lcpp/issues/20)

--- lcpp.lua
+++ lcpp.lua
@@ -1268,7 +1268,7 @@ local function parseFunction(state, input)
 		
 	-- build macro funcion
 	local func = function(input)
-		return input:gsub(name.."%s*(%b())", function (match)
+		return input:gsub("^"..name.."%s*(%b())", function (match)
 			return replaceArgs(match, repl)
 		end)
 	end
